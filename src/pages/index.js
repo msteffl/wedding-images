5969de504e4d167ea094c3124daff744ddefc54d
smf2abt <michael.steffl2@bosch.com>
2022-05-15 10:18:41 +0200

    feat: filestack

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,28 +4,22 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { PickerInline } from "filestack-react"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hochzeit Valentina und Michael</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
+const apikey = "APtxwk5KVRsy0bq7q4ev1z"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h1>Hochzeit Valentina und Michael</h1>
+      <PickerInline
+        apikey={apikey}
+        onSuccess={res => console.log(res)}
+        onUploadDone={res => console.log(res)}
+      />
+    </Layout>
+  )
+}
 
 export default IndexPage
